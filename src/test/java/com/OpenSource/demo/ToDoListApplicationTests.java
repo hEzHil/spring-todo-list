a559cb3bd86bf425468d2a0e5d50b0b03d0875fc
hEzHil <88553060+hEzHil@users.noreply.github.com>
2024-08-19 09:24:17 +0530

    Initial setup

---
+++ src/test/java/com/OpenSource/demo/ToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.OpenSource.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
